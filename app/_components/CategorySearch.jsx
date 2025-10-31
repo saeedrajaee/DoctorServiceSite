05837427e139cb16dd6390aaca9fc0969c269e86
Saeed Rajaee <rajaee_saeed@yahoo.com>
2025-10-31 14:48:31 +0330

    initial commit

--- app/_components/CategorySearch.jsx
+++ app/_components/CategorySearch.jsx
@@ -1,11 +1,19 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { doctorCategory } from "@/data/data";
+import Image from "next/image";
 
 function CategorySearch() {
+  const [categoryList, setCategoryList] = useState();
+  useEffect(() => {
+    getCategoryList;
+  }, []);
+  const getCategoryList = doctorCategory;
   return (
-    <div className=" mb-10 items-center flex flex-col gap-4">
+    <div className=" mb-10 items-center px-5 flex flex-col gap-4">
       <h2 className=" font-bold text-4xl tracking-wide">
         Search
         <span className=" text-blue-600"> Doctors</span>
@@ -19,6 +27,23 @@ function CategorySearch() {
           <Search className=" h-4 w-4 mr-2" />
           Search
         </Button>
+      </div>
+      {/* Dispaly List of Category */}
+      <div className=" grid grid-cols-2 mt-5ّ md:grid-cols-3 lg:grid-cols-4">
+        {doctorCategory.map((data, i) => {
+          return (
+            <div
+              key={data.id}
+              className="flex flex-col text-center
+               items-center p-5 bg-blue-50 m-2
+               rounded-lg gap-2 hover:scale-110
+                transition-all ease-in-out cursor-pointer"
+            >
+              <Image src={data.image} alt="icon" width={40} height={40} />
+              <label className=" text-blue-600 text-sm">{data.name}</label>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,8 +1,10 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Hero = () => {
+
   return (
     <section>
       <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
